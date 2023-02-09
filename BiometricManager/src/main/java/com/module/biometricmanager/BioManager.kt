5f1b66b63ee55fb66693b30274c167f1b39274ea
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-02-09 17:25:30 +0900

    feat: 지문 인식 성공

--- BiometricManager/src/main/java/com/module/biometricmanager/BioManager.kt
+++ BiometricManager/src/main/java/com/module/biometricmanager/BioManager.kt
@@ -5,15 +5,18 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.module.biometricmanager.util.LogUtil
 
-class BioManager(private val context: Context) {
+class BioManager(private val activity: AppCompatActivity) {
     /**
      * 생체 인증이 가능한지 확인한다.
      */
     fun canAuthenticateByBioMetric() {
-        val canAuthenticate = BiometricManager.from(context).canAuthenticate(
+        val canAuthenticate = BiometricManager.from(activity).canAuthenticate(
             BiometricManager.Authenticators.BIOMETRIC_STRONG or
                         BiometricManager.Authenticators.DEVICE_CREDENTIAL
         )
@@ -21,6 +24,7 @@ class BioManager(private val context: Context) {
             //생체 인증 가능
             BiometricManager.BIOMETRIC_SUCCESS -> {
                 LogUtil.logD("App can authenticate using biometrics.")
+                showBiometricPrompt(activity)
             }
 
             //기기에서 생체 인증을 지원하지 않는 경우
@@ -36,9 +40,59 @@ class BioManager(private val context: Context) {
             //생체 인식 정보가 등록되어 있지 않은 경우
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
                 LogUtil.logD("Prompts the user to create credentials that your app accepts.")
-                showSecuritySettingDialog(context)
+                showSecuritySettingDialog(activity)
             }
         }
+    }
+
+
+    fun showBiometricPrompt(activity: AppCompatActivity) {
+        val biometricPrompt = createBiometricPrompt(activity)
+        val promptInfo = createPromptInfo()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+
+    fun createBiometricPrompt(activity: AppCompatActivity): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(activity)
+
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            //지문 인식 ERROR
+            override fun onAuthenticationError(errCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errCode, errString)
+                LogUtil.logD("errCode is $errCode and errString is: $errString")
+            }
+
+            //"지문 인식 실패"
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                LogUtil.logD("User biometric rejected.")
+            }
+
+            //"지문 인식 성공"
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                LogUtil.logD("Authentication was successful")
+            }
+        }
+
+        return BiometricPrompt(activity, executor, callback)
+    }
+
+    /**
+     * 지문 인식 프롬프트
+     */
+    fun createPromptInfo(): BiometricPrompt.PromptInfo {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder().apply {
+            setTitle("Sample App Authentication")
+            setSubtitle("Please login to get access")
+            setDescription("Sample App is using Android biometric authentication")
+            setConfirmationRequired(false)
+            setNegativeButtonText("close")
+        }.build()
+
+        return promptInfo
     }
 
     /**
@@ -66,6 +120,8 @@ class BioManager(private val context: Context) {
             LogUtil.logD("ACTION_BIOMETRIC_ENROLL 지원안하는 버전")
         }
     }
+
+
 }
 
 
--- app/src/main/java/com/ex/app/MainActivity.kt
+++ app/src/main/java/com/ex/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ex.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ex.app.databinding.ActivityMainBinding
 import com.module.biometricmanager.BioManager
