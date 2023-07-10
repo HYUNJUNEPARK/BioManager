251a208ced09a9db74fb8172f82708eb6306da88
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-07-10 10:45:20 +0900

    commit

--- app/src/main/java/com/ex/app/MainActivity.kt
+++ app/src/main/java/com/ex/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ex.app
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.media.MediaDrm.ErrorCodes
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
